b5fab28032ddda51aaa5b9eff2a8de9e1a6fc9df
Wendell Diniz <wendellcien@gmail.com>
2022-04-09 18:53:39 -0300

    Final config for deploy.

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 export default {
-  url: 'http://localhost:1337/api/pages?',
+  /* url: 'http://localhost:1337/api/pages?', */
+  url: 'https://my-react-landing-page.herokuapp.com/api/pages?',
   siteName: 'SunlightIO',
   defaultSlug: 'sunlight-io',
 };
